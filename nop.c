11bec8110a79d6d0f8c0a1d2f7d3a1013fa25a81
James gitere <sasmuypwa2019.com>
2023-12-14 15:28:56 +0300

    Does nothing.

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+* monty_nop - Does nothing.
+* @stack: A pointer to the top of the stack.
+* @counter: The line number in Monty file where nop was called.
+*/
+void monty_nop(stack_t **stack, unsigned int counter)
+{
+	(void)stack;
+	(void)counter;
+	/* No operation */
+}
