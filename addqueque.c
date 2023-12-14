bcd7041b9a0ef54b7faf788eb00fb06e81acaa93
James gitere <sasmuypwa2019.com>
2023-12-14 20:11:48 +0300

    add node to the tail stack

---
+++ addqueque.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+/**
+* addqueue - add node to the tail stack
+* @node_value: new_value
+* @head: head of the stack
+* Return: no return
+*/
+void addqueue(stack_t **head, int node_value)
+{
+	stack_t *new_node, *current;
+
+	current = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = node_value;
+	new_node->next = NULL;
+	if (current)
+	{
+		while (current->next)
+			current = current->next;
+	}
+	if (!current)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		current->next = new_node;
+		new_node->prev = current;
+	}
+}
