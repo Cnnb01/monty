01b99028c81b0316a1d531fb4618596c1f43dd42
James gitere <sasmuypwa2019.com>
2023-12-14 10:05:09 +0300

    function that adds the top two elements of the stack.

---
+++ add.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+* add - Adds the top two elements of the stack.
+* @head: Pointer to the head of the stack.
+* @counter: Line number of the opcode in the Monty script.
+*/
+void add(stack_t **head, unsigned int counter)
+{
+	stack_t *temp;
+	int len = 0;
+
+	temp = *head;
+	while (temp)
+	{
+		temp = temp->next;
+		len++;
+	}
+
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+		fclose(config.file);
+		free(config.content);
+		clear_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *head;
+	temp->next->n += temp->n;
+	*head = temp->next;
+	(*head)->prev = NULL;
+	free(temp);
+}
