5f6acd637ca215e164c20682901fa96f927cdc8e
James gitere <sasmuypwa2019.com>
2023-12-14 20:13:21 +0300

    prints the stack

---
+++ pall.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+* monty_pall - prints the stack
+* @head: stack head
+* @counter: not used
+* Return: no return
+*/
+void monty_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *current;
+
+	(void)counter;
+
+	current = *head;
+	if (current == NULL)
+		return;
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
