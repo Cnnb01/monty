d4f6f3d1f81aa8a69d48e82d93a3e8afd4160ade
James gitere <sasmuypwa2019.com>
2023-12-14 20:13:54 +0300

    Prints the value at the top of the stack.

---
+++ pint.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * monty_pint - Prints the value at the top of the stack.
+ * @head: Pointer to the stack's top
+ * @counter: Line number in the Monty bytecode file
+ * Return: No return value
+ */
+void monty_pint(stack_t **head, unsigned int counter)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fclose(config.file);
+		free(config.content);
+		clear_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
