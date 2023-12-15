5654b859c0de549a0cad8d6cb3eab0220283ab16
James gitere <sasmuypwa2019.com>
2023-12-15 17:40:53 +0300

    prints the top

---
+++ queue.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * monty_queque - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void monty_queque(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	config.lifi = 1;
+}
---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * monty_stack - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void monty_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	config.lifi = 0;
+}
